09d8199d83964845a914a92a97019221d5e1fcea
최지우 <cjw@choejiuui-MacBookPro.local>
2023-02-13 17:20:57 +0900

    [Setting] #1 Add Extension, Util File

---
+++ EatSSU-iOS/EatSSU-iOS/Global/Extension/UIColor+.swift
@@ -0,0 +1,24 @@
+//
+//  UIColor+.swift
+//  EatSSU-iOS
+//
+//  Created by 최지우 on 2023/02/13.
+//
+
+import Foundation
+
+import UIKit
+import SnapKit
+
+enum CustomColor {
+    
+}
+
+extension UIColor {
+    
+    static func customColor(_ color: CustomColor) -> UIColor {
+            switch color {
+            
+            }
+        }
+}
---
+++ EatSSU-iOS/EatSSU-iOS/Global/Extension/UIViewController+.swift
@@ -0,0 +1,27 @@
+//
+//  UIViewController+.swift
+//  EatSSU-iOS
+//
+//  Created by 최지우 on 2023/02/13.
+//
+
+import SwiftUI
+
+#if DEBUG
+extension UIViewController {
+    private struct Preview: UIViewControllerRepresentable {
+            let viewController: UIViewController
+
+            func makeUIViewController(context: Context) -> UIViewController {
+                return viewController
+            }
+
+            func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+            }
+        }
+
+        func toPreview() -> some View {
+            Preview(viewController: self)
+        }
+}
+#endif
