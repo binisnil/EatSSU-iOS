2000e6b160386ec5d34549654b1b4e339d5da28c
최지우 <cjw@choejiuui-MacBookPro.local>
2023-02-13 16:55:13 +0900

    [Setting] #1 Project Setting

---
+++ EatSSU-iOS/EatSSU-iOS/SceneDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  SceneDelegate.swift
+//  EatSSU-iOS
+//
+//  Created by 최지우 on 2023/02/13.
+//
+
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        window = UIWindow(windowScene: windowScene)
+        window?.windowScene = windowScene
+        let navigationController = UINavigationController(rootViewController: ViewController())
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+
+    }
+}
---
+++ EatSSU-iOS/EatSSU-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EatSSU-iOS
+//
+//  Created by 최지우 on 2023/02/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
