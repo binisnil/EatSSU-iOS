d90b0e44f61e8285166c072552f58c7b047196c2
최지우 <cjw@choejiuui-MacBookPro.local>
2023-03-28 03:14:17 +0900

    [Add] #3 MorningView 추가

---
+++ EatSSU-iOS/EatSSU-iOS/Screen/Home/View/MorningView.swift
@@ -0,0 +1,62 @@
+//
+//  MorningView.swift
+//  EatSSU-iOS
+//
+//  Created by 최지우 on 2023/03/28.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class MorningView : UIView {
+    
+    // MARK: - UI Components
+    
+    private let coordinateImage = UIImageView().then {
+        $0.image = UIImage(named: "coordinate")
+    }
+    
+    let studentRestaurantLabel = UILabel().then {
+        $0.text = "학생 식당"
+        $0.font = UIFont.boldSystemFont(ofSize: 20)
+    }
+    
+    //StackView
+    
+    lazy var restaurantTitleStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [coordinateImage, studentRestaurantLabel])
+        stackView.axis = .horizontal
+        stackView.spacing = 10
+        stackView.alignment = .leading
+        return stackView
+    }()
+  
+    // MARK: - init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Func
+    func configureUI() {
+        self.addSubviews(restaurantTitleStackView)
+    }
+    
+    func setLayout() {
+        studentRestaurantLabel.snp.makeConstraints {
+            $0.leading.equalTo(coordinateImage.snp.trailing).offset(10)
+        }
+        restaurantTitleStackView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(160)
+            $0.leading.equalToSuperview().offset(21)
+        }
+    }
+    
+}
