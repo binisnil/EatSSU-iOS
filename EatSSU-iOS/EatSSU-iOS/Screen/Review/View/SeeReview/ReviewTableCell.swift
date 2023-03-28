16abf3cf1069f32f1e50bd001b274c7cd7c71a18
binisnull <dbsqlssss0704@gmail.com>
2023-03-29 02:26:38 +0900

    [FEAT]: 사진 제외된 ReviewViewController 구현 완료 #5

--- EatSSU-iOS/EatSSU-iOS/Screen/Review/View/SeeReview/ReviewTableCell.swift
+++ EatSSU-iOS/EatSSU-iOS/Screen/Review/View/SeeReview/ReviewTableCell.swift
@@ -35,15 +35,15 @@ class ReviewTableCell: UITableViewCell {
         return UIImage(named: "starEmpty.svg")
     }()
     
-    var starNumber: Int = 5 {
-        didSet { setStarButtons() }
-    }
+//    var starNumber: Int = 5 {
+//        didSet { setStarButtons() }
+//    }
     
     private var dateLabel: UILabel = {
         let label = UILabel()
         label.text = "2023.03.03"
-        label.font = .regular(size: 16)
-        label.textColor = .mediumGray
+        label.font = .regular(size: 12)
+        label.textColor = .darkGray
         return label
     }()
     
@@ -77,25 +77,162 @@ class ReviewTableCell: UITableViewCell {
         return label
     }()
     
+    var firstUserTag: UIButton = {
+        let button = UIButton()
+        button.setTitle("든든한 한 끼", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .lightGray
+        button.layer.cornerRadius = 8
+        button.layer.borderWidth = 0.2
+        button.layer.borderColor = UIColor.black.cgColor
+        button.titleLabel?.font = .medium(size: 11)
+        return button
+    }()
+    
+    var secondUserTag: UIButton = {
+        let button = UIButton()
+        button.setTitle("그럭저럭해요", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .lightGray
+        button.layer.cornerRadius = 8
+        button.layer.borderWidth = 0.2
+        button.layer.borderColor = UIColor.black.cgColor
+        button.titleLabel?.font = .medium(size: 11)
+        return button
+    }()
+    
+    var thirdUserTag: UIButton = {
+        let button = UIButton()
+        button.setTitle("아쉬워요", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .lightGray
+        button.layer.cornerRadius = 8
+        button.layer.borderWidth = 0.2
+        button.layer.borderColor = UIColor.black.cgColor
+        button.titleLabel?.font = .medium(size: 11)
+        return button
+    }()
+    
+    var reviewTextView : UITextView = {
+        let textView = UITextView()
+        textView.textColor = UIColor.black
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        textView.backgroundColor = .systemBackground
+        textView.font = .regular(size: 16)
+        textView.text = "여기 계란국 맛집임... 김치볶음밥에 계란후라이 없어서 아쉽 다음에 또 먹어야지"
+
+        return textView
+    }()
+    
+    lazy var rateDateStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [starStackView, dateLabel])
+        stackView.axis = .horizontal
+        stackView.spacing = 10
+        stackView.alignment = .fill
+        return stackView
+    }()
+    
+    lazy var infoStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [userNameLabel, rateDateStackView])
+        stackView.axis = .vertical
+        stackView.spacing = 3
+        stackView.alignment = .leading
+        return stackView
+    }()
+    
+    lazy var profileStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [userProfileImageView, infoStackView])
+        stackView.axis = .horizontal
+        stackView.spacing = 11
+        stackView.alignment = .leading
+        return stackView
+    }()
+    
+    lazy var userTagStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [firstUserTag, secondUserTag, thirdUserTag])
+        stackView.axis = .horizontal
+        stackView.spacing = 19
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        return stackView
+    }()
+    
+    lazy var contentStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [reviewTextView,userTagStackView])
+        stackView.axis = .vertical
+        stackView.spacing = 6
+        stackView.alignment = .leading
+        return stackView
+    }()
+    
     //MARK: - Functions
     
-    func setStarButtons(){
-        for i in 0..<5 {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.contentView.addSubview(self.profileStackView)
+        self.contentView.addSubview(self.sideButton)
+        self.contentView.addSubview(self.contentStackView)
+        setLayout()
+        setStarButtons()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setLayout() {
+        profileStackView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(5)
+            make.leading.equalToSuperview().offset(16)
+        }
+        sideButton.snp.makeConstraints { make in
+            make.centerY.equalTo(profileStackView.snp.top).offset(3)
+            make.trailing.equalToSuperview().offset(-16)
+        }
+        contentStackView.snp.makeConstraints { make in
+            make.top.equalTo(profileStackView.snp.bottom).offset(7)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+            make.bottom.equalToSuperview().offset(-15)
+        }
+        
+        firstUserTag.snp.makeConstraints { make in
+            make.height.equalTo(19)
+        }
+        
+        secondUserTag.snp.makeConstraints { make in
+            make.height.equalTo(19)
+        }
+        
+        thirdUserTag.snp.makeConstraints { make in
+            make.height.equalTo(19)
+        }
+    }
+    
+    func setStarButtons() {
+        for i in 0...personalRate - 1 {
             let button = UIButton()
-            button.setImage(starEmptyImage, for: .normal)
+            button.setImage(starFillImage, for: .normal)
+            button.snp.makeConstraints { make in
+                make.height.equalTo(11.19)
+                make.width.equalTo(11.71)
+            }
             button.tag = i
             starButtons += [button]
             starStackView.addArrangedSubview(button)
         }
-    }
-    
-    func howManyStars(rate: Int) {
-        let end = rate
-        for i in 0...end {
-            starButtons[i].setImage(starFillImage, for: .normal)
-        }
-        for i in end + 1..<starNumber {
-            starButtons[i].setImage(starEmptyImage, for: .normal)
+        
+        for i in personalRate..<5 {
+            let button = UIButton()
+            button.setImage(starEmptyImage, for: .normal)
+            button.snp.makeConstraints { make in
+                make.height.equalTo(11.19)
+                make.width.equalTo(11.71)
+            }
+            button.tag = i
+            starButtons += [button]
+            starStackView.addArrangedSubview(button)
         }
     }
 }
--- EatSSU-iOS/EatSSU-iOS/Screen/Review/ViewController/ReviewViewController.swift
+++ EatSSU-iOS/EatSSU-iOS/Screen/Review/ViewController/ReviewViewController.swift
@@ -16,12 +16,41 @@ class ReviewViewController: BaseViewController {
     
     let topReviewView = ReviewRateView()
     
+    private let progressView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .backgroundGray
+        return view
+    }()
+    
+    let reviewLabel: UILabel = {
+        let label = UILabel()
+        label.text = "리뷰"
+        label.font = .bold(size: 18)
+        label.textColor = .black
+        return label
+    }()
+    
+    let reviewTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.separatorStyle = .none
+        tableView.showsVerticalScrollIndicator = false
+        return tableView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        reviewTableView.register(ReviewTableCell.self, forCellReuseIdentifier: ReviewTableCell.identifier)
+        reviewTableView.delegate = self
+        reviewTableView.dataSource = self
+    }
+    
     //MARK: - Function
 
     override func configureUI() {
-        view.backgroundColor = .backgroundGray
+        view.backgroundColor = .systemBackground
         topReviewView.backgroundColor = .white
-        view.addSubviews(topReviewView)
+        reviewTableView.backgroundColor = .white
+        view.addSubviews(topReviewView, progressView, reviewLabel, reviewTableView)
     }
     
     override func setLayout() {
@@ -29,6 +58,21 @@ class ReviewViewController: BaseViewController {
             make.top.equalToSuperview()
             make.leading.trailing.equalToSuperview()
             make.height.equalTo(350)
+        }
+        progressView.snp.makeConstraints { make in
+            make.top.equalTo(topReviewView.snp.bottom)
+            make.width.equalToSuperview()
+            make.height.equalTo(8)
+        }
+        reviewLabel.snp.makeConstraints { make in
+            make.top.equalTo(progressView.snp.bottom).offset(20)
+            make.leading.equalToSuperview().offset(16)
+        }
+        
+        reviewTableView.snp.makeConstraints { make in
+            make.top.equalTo(reviewLabel.snp.bottom).offset(15)
+            make.bottom.equalToSuperview()
+            make.leading.trailing.equalToSuperview()
         }
     }
     
@@ -39,5 +83,22 @@ class ReviewViewController: BaseViewController {
     @objc func userTapReviewButton() {
         let nextVC = SetRateViewController()
         self.navigationController?.pushViewController(nextVC, animated: true)
+    }
+}
+
+extension ReviewViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("cell did touched")
+    }
+}
+
+extension ReviewViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ReviewTableCell.identifier, for: indexPath) as? ReviewTableCell ?? ReviewTableCell()
+        return cell
     }
 }
