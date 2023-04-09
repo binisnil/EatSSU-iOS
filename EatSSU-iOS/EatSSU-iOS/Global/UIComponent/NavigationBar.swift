0732140e80ebee589a8b0a309868e50aaf6eafd1
CJiu01 <jiwoo020801@gmail.com>
2023-04-10 00:00:27 +0900

    [Feat] NaviBar test

--- EatSSU-iOS/EatSSU-iOS/Global/UIComponent/NavigationBar.swift
+++ EatSSU-iOS/EatSSU-iOS/Global/UIComponent/NavigationBar.swift
@@ -5,4 +5,61 @@
 //  Created by 최지우 on 2023/04/08.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+class NavigationBar: BaseViewController {
+    
+//    // MARK: - Properties
+//
+//    private lazy var titleLabel = UILabel()
+//    private lazy var backButton = UIBarButtonItem()
+//    private lazy var rightButton = UIBarButtonItem()
+//
+//    // MARK: - UI Components
+//
+//    // MARK: - Life Cycles
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        view.backgroundColor = .systemBackground
+//        configureUI()
+//        setLayout()
+//        setButtonEvent()
+//    }
+//
+//    //MARK: - Functions
+//
+//    override func configureUI() {
+//        //override Point
+//    }
+//
+//    override func setLayout() {
+//        //override Point
+//    }
+//}
+//
+//// MARK: - Custom Methods
+//extension NavigationBar {
+//
+//    /// title 지정 함수
+//    func setTitle(_ title: String) {
+//        titleLabel.text = title
+//    }
+    
+}
+
+
+//func customNavigationBar() {
+//        navigationController?.navigationBar.tintColor = .primary
+//        navigationController?.navigationBar.barTintColor = .white
+//
+//        let backButton: UIBarButtonItem = UIBarButtonItem()
+//        backButton.title = ""
+//        navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
+//
+//        navigationItem.title = "회원가입"
+//        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.primary, NSAttributedString.Key.font: UIFont.bold(size: 22)]
+//    }
