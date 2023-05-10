60c032107d12e61843d5a5906409332da846521d
binisnull <dbsqlssss0704@gmail.com>
2023-05-10 23:25:52 +0900

    [FEAT] #15 공지사항 UI 구현 완료

--- EatSSU-iOS/EatSSU-iOS/Global/Base/BaseViewController.swift
+++ EatSSU-iOS/EatSSU-iOS/Global/Base/BaseViewController.swift
@@ -14,13 +14,27 @@ class BaseViewController: UIViewController {
     
     // MARK: - Properties
     
-    // MARK: - UI Components
+    lazy private(set) var className: String = {
+      return type(of: self).description().components(separatedBy: ".").last ?? ""
+    }()
     
-    // MARK: - Life Cycles
+    // MARK: - Initializing
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    deinit {
+        print("DEINIT: \(className)")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
         configureUI()
         setLayout()
         setButtonEvent()
@@ -30,6 +44,8 @@ class BaseViewController: UIViewController {
     
     func configureUI() {
         //override Point
+        view.backgroundColor = .systemBackground
+
     }
     
     func setLayout() {
@@ -40,4 +56,5 @@ class BaseViewController: UIViewController {
         //override Point
     }
 }
+
 
