a962907c5bba820225b1d76b6d78615c7650322c
CJiu01 <jiwoo020801@gmail.com>
2023-05-16 00:43:36 +0900

    [Feat] #19 버튼 제외 UI 완료

--- EatSSU-iOS/EatSSU-iOS/Screen/Review/ViewController/DeclarationViewController.swift
+++ EatSSU-iOS/EatSSU-iOS/Screen/Review/ViewController/DeclarationViewController.swift
@@ -15,39 +15,63 @@ class DeclarationViewController: BaseViewController {
     // MARK: - Properties
     
     // MARK: - UI Components
-    let alertDeclarationLabel = UILabel().then {
+    private let alertDeclarationLabel = UILabel().then {
         $0.text = "리뷰를 신고하는 이유를 선택해주세요."
+        $0.font = .semiBold(size: 18)
     }
     
-    let sendButton = MainButton().then {
-        $0.title = "EAT SSU에게 보내기"
-    }
-    
-    let report1Label = UILabel().then {
+    private let report1Label = UILabel().then {
         $0.text = "메뉴와 관련없는 내용"
     }
 
-    let report2Label = UILabel().then {
+    private let report2Label = UILabel().then {
         $0.text = "음란성, 욕설 등 부적절한 내용"
     }
     
-    let report3Label = UILabel().then {
+    private let report3Label = UILabel().then {
         $0.text = "부적절한 홍보 또는 광고"
     }
     
-    let report4Label = UILabel().then {
+    private let report4Label = UILabel().then {
         $0.text = "리뷰 작성 취지에 맞지 않는 내용 (복사글 등)"
     }
     
-    let report5Label = UILabel().then {
+    private let report5Label = UILabel().then {
         $0.text = "저작권 도용 의심 (사진 등)"
     }
     
-    let report6Label = UILabel().then {
+    private let report6Label = UILabel().then {
         $0.text = "기타 (하단 내용 작성)"
     }
     
+    lazy var reportLabelStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [report1Label, report2Label, report3Label, report4Label, report5Label, report6Label])
+        stackView.axis = .vertical
+        stackView.spacing = 16
+        stackView.alignment = .leading
+        return stackView
+    }()
     
+    private let userReportTextView: UITextView = {
+        let textView = UITextView()
+        textView.font = .medium(size: 16)
+        textView.layer.cornerRadius = 10
+        textView.backgroundColor = .backgroundGray
+        textView.layer.borderWidth = 1
+        textView.layer.borderColor = UIColor.darkGray.cgColor
+        textView.textContainerInset = UIEdgeInsets(top: 16.0, left: 16.0, bottom: 16.0, right: 16.0)
+        return textView
+    }()
+    
+    private let textLimitLabel = UILabel().then {
+        $0.text = "최대 150자"
+        $0.font = .medium(size: 12)
+        $0.textColor = .blackGray
+    }
+    
+    private let sendButton = MainButton().then {
+        $0.title = "EAT SSU에게 보내기"
+    }
     
     // MARK: - Life Cycles
     
@@ -64,7 +88,16 @@ class DeclarationViewController: BaseViewController {
     
     override func configureUI() {
         view.addSubviews(alertDeclarationLabel,
-                         sendButton)
+                         reportLabelStackView,
+                         userReportTextView,
+                         textLimitLabel,
+                         sendButton
+                         )
+        
+        [report1Label,report2Label,report3Label,report4Label,report5Label, report6Label].forEach {
+            $0.font = .medium(size: 16)
+            $0.textColor = .blackGray
+        }
         
     }
     
@@ -72,6 +105,20 @@ class DeclarationViewController: BaseViewController {
         alertDeclarationLabel.snp.makeConstraints {
             $0.top.equalToSuperview().offset(107)
             $0.leading.equalToSuperview().offset(16)
+        }
+        reportLabelStackView.snp.makeConstraints {
+            $0.top.equalTo(alertDeclarationLabel.snp.bottom).offset(22)
+            $0.leading.equalToSuperview().offset(16)
+        }
+        userReportTextView.snp.makeConstraints {
+            $0.top.equalTo(reportLabelStackView.snp.bottom).offset(29)
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.equalToSuperview().offset(-16)
+            $0.height.equalTo(181)
+        }
+        textLimitLabel.snp.makeConstraints {
+            $0.top.equalTo(userReportTextView.snp.bottom).offset(3)
+            $0.trailing.equalToSuperview().inset(16)
         }
         sendButton.snp.makeConstraints {
             $0.bottom.equalToSuperview().offset(-61)
