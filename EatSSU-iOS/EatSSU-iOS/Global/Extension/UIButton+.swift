8bea95701edb9dccbd2cd627b39fa86c2bcae79e
CJiu01 <jiwoo020801@gmail.com>
2023-06-27 01:35:10 +0900

    [Feat] #49 UIButton setRoundBorder Extension

--- EatSSU-iOS/EatSSU-iOS/Global/Extension/UIButton+.swift
+++ EatSSU-iOS/EatSSU-iOS/Global/Extension/UIButton+.swift
@@ -5,4 +5,15 @@
 //  Created by 최지우 on 2023/06/27.
 //
 
-import Foundation
+import UIKit
+
+extension UIButton {
+    
+    func setRoundBorder(borderColor: UIColor, borderWidth: CGFloat, cornerRadius: CGFloat) {
+        layer.masksToBounds = true
+        layer.borderColor = borderColor.cgColor
+        layer.borderWidth = borderWidth
+        layer.cornerRadius = cornerRadius
+    }
+    
+}
