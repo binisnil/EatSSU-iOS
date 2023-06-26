7ecba3f1aa826a48eb0c8a29e1376ae678995548
CJiu01 <jiwoo020801@gmail.com>
2023-06-27 01:47:37 +0900

    [Feat] #48 stringLiteral 정리

---
+++ EatSSU-iOS/EatSSU-iOS/Global/Literal/TextLiteral.swift
@@ -0,0 +1,17 @@
+//
+//  TextLiteral.swift
+//  EatSSU-iOS
+//
+//  Created by 최지우 on 2023/06/27.
+//
+
+import UIKit
+
+enum TextLiteral {
+    
+    // MARK: - sign in
+    static let signInWithApple: String = "Apple 로그인"
+    static let signInWithKakao: String = "카카오 로그인"
+    static let lookingWithNoSignIn: String = "둘러보기"
+    
+}
