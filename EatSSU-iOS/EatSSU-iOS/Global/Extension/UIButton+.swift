ef00a394dff7586891808b4dba0acd68f6207709
CJiu01 <102797359+CJiu01@users.noreply.github.com>
2023-07-05 16:10:48 +0900

    [Refacor] UIButton+.swift 주석 추가

--- EatSSU-iOS/EatSSU-iOS/Global/Extension/UIButton+.swift
+++ EatSSU-iOS/EatSSU-iOS/Global/Extension/UIButton+.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 extension UIButton {
-    
+
+    /// Button 상태에 따른 Color 지정
     func setBackgroundColor(_ color: UIColor, for state: UIControl.State) {
         let minimumSize: CGSize = CGSize(width: 1.0, height: 1.0)
         
@@ -25,13 +26,15 @@ extension UIButton {
         self.clipsToBounds = true
         self.setBackgroundImage(colorImage, for: state)
     }
-    
+
+    /// Button 타이틀 속성 지정
     func addTitleAttribute(title: String, titleColor: UIColor, fontName: UIFont) {
         self.setTitle(title, for: .normal)
         self.setTitleColor(titleColor, for: .normal)
         self.titleLabel?.font = fontName
     }
-    
+
+    /// Button border 속성 지정
     func setRoundBorder(borderColor: UIColor, borderWidth: CGFloat, cornerRadius: CGFloat) {
         layer.masksToBounds = true
         layer.borderColor = borderColor.cgColor
