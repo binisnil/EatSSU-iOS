01579b04c706d4b6a78af7e2be1a01590038bc51
CJiu01 <jiwoo020801@gmail.com>
2023-08-09 18:10:24 +0900

    [FEAT] #85 BaseTableViewCell 구현

---
+++ EatSSU-iOS/EatSSU-iOS/Global/Base/BaseTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  BaseTableViewCell.swift
+//  EatSSU-iOS
+//
+//  Created by 최지우 on 2023/08/09.
+//
+
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {
+        selectionStyle = .none
+    }
+    
+}
