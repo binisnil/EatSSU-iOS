1793421d59e8451166079ea56f1926c2d2486dc4
CJiu01 <jiwoo020801@gmail.com>
2023-08-09 18:07:08 +0900

    [CHORE] #85 이전 파일 주석 처리

--- EatSSU-iOS/EatSSU-iOS/Screen/Home/ViewController/HomeComponent/LunchViewController.swift
+++ EatSSU-iOS/EatSSU-iOS/Screen/Home/ViewController/HomeComponent/LunchViewController.swift
@@ -10,78 +10,78 @@ import UIKit
 import SnapKit
 import Moya
 
-class LunchViewController: BaseViewController {
-    
-    //MARK: - Properties
-    
-    let morningTableProvider = MoyaProvider<HomeRouter>()
-    let tabBarHeight: CGFloat = 50
-    
-    //MARK: - UI Components
-    
-    private let contentView = UIView()
-    let lunchView = HomeRestaurantView()
-    let scrollView = UIScrollView().then {
-        $0.backgroundColor = .systemBackground
-        $0.showsVerticalScrollIndicator = false
-    }
-    
-    //MARK: - Life Cycles
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        lunchView.delegate = self
-        
-        setButtonEvent()        
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        fixedMenuAPI()
-    }
-    
-    //MARK: - Functions
-    
-    override func configureUI() {
-        view.addSubviews(scrollView)
-        scrollView.addSubview(contentView)
-        contentView.addSubviews(lunchView)
-    }
-
-    override func setLayout() {
-        scrollView.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(tabBarHeight)
-            
-            $0.horizontalEdges.bottom.equalToSuperview()
-            $0.bottom.equalToSuperview()
-        }
-        contentView.snp.makeConstraints {
-            $0.edges.width.equalToSuperview()
-        }
-        lunchView.snp.makeConstraints {
-            $0.top.leading.bottom.trailing.equalToSuperview()
-        }
-    }
-
-    func lunchMenuAPI(date: String) {
-        lunchView.getChangeMenuTableResponse(date: date, restaurant: "DOMITORY", time: "LUNCH")
-        lunchView.getChangeMenuTableResponse(date: date, restaurant: "DODAM", time: "LUNCH")
-        lunchView.getChangeMenuTableResponse(date: date, restaurant: "HAKSIK", time: "LUNCH")
-    }
-    
-    func fixedMenuAPI() {
-        lunchView.getFixedMenuTableResponse(restaurant: "FOOD_COURT")
-        lunchView.getFixedMenuTableResponse(restaurant: "SNACK_CORNER")
-        lunchView.getFixedMenuTableResponse(restaurant: "THE_KITCHEN")
-    }
-}
-
-extension LunchViewController: TableViewCellSelectionDelegate {
-    func didSelectCell() {
-        let reviewViewController = ReviewViewController()
-        self.navigationController?.pushViewController(reviewViewController, animated: true)
-        lunchView.delegateMenu = reviewViewController
-    }
-    
-}
+//class LunchViewController: BaseViewController {
+//
+//    //MARK: - Properties
+//
+//    let morningTableProvider = MoyaProvider<HomeRouter>()
+//    let tabBarHeight: CGFloat = 50
+//
+//    //MARK: - UI Components
+//
+//    private let contentView = UIView()
+//    let lunchView = HomeRestaurantView()
+//    let scrollView = UIScrollView().then {
+//        $0.backgroundColor = .systemBackground
+//        $0.showsVerticalScrollIndicator = false
+//    }
+//
+//    //MARK: - Life Cycles
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        lunchView.delegate = self
+//
+//        setButtonEvent()
+//    }
+//
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//
+//        fixedMenuAPI()
+//    }
+//
+//    //MARK: - Functions
+//
+//    override func configureUI() {
+//        view.addSubviews(scrollView)
+//        scrollView.addSubview(contentView)
+//        contentView.addSubviews(lunchView)
+//    }
+//
+//    override func setLayout() {
+//        scrollView.snp.makeConstraints {
+//            $0.top.equalToSuperview().offset(tabBarHeight)
+//
+//            $0.horizontalEdges.bottom.equalToSuperview()
+//            $0.bottom.equalToSuperview()
+//        }
+//        contentView.snp.makeConstraints {
+//            $0.edges.width.equalToSuperview()
+//        }
+//        lunchView.snp.makeConstraints {
+//            $0.top.leading.bottom.trailing.equalToSuperview()
+//        }
+//    }
+//
+//    func lunchMenuAPI(date: String) {
+//        lunchView.getChangeMenuTableResponse(date: date, restaurant: "DOMITORY", time: "LUNCH")
+//        lunchView.getChangeMenuTableResponse(date: date, restaurant: "DODAM", time: "LUNCH")
+//        lunchView.getChangeMenuTableResponse(date: date, restaurant: "HAKSIK", time: "LUNCH")
+//    }
+//
+//    func fixedMenuAPI() {
+//        lunchView.getFixedMenuTableResponse(restaurant: "FOOD_COURT")
+//        lunchView.getFixedMenuTableResponse(restaurant: "SNACK_CORNER")
+//        lunchView.getFixedMenuTableResponse(restaurant: "THE_KITCHEN")
+//    }
+//}
+//
+//extension LunchViewController: TableViewCellSelectionDelegate {
+//    func didSelectCell() {
+//        let reviewViewController = ReviewViewController()
+//        self.navigationController?.pushViewController(reviewViewController, animated: true)
+//        lunchView.delegateMenu = reviewViewController
+//    }
+//
+//}
--- EatSSU-iOS/EatSSU-iOS/Screen/Review/ViewController/ReviewViewController.swift
+++ EatSSU-iOS/EatSSU-iOS/Screen/Review/ViewController/ReviewViewController.swift
@@ -195,10 +195,10 @@ extension ReviewViewController {
     }
 }
 
-extension ReviewViewController: BindCellMenuTypeInfoDelegate {
-    func didBindMenuTypeInfo(menuTypeInfo: MenuTypeInfo) {
-        type = menuTypeInfo.menuType
-        menuID = menuTypeInfo.menuID
-        menuIDList = menuTypeInfo.menuIDList ?? []
-    }
-}
+//extension ReviewViewController: BindCellMenuTypeInfoDelegate {
+//    func didBindMenuTypeInfo(menuTypeInfo: MenuTypeInfo) {
+//        type = menuTypeInfo.menuType
+//        menuID = menuTypeInfo.menuID
+//        menuIDList = menuTypeInfo.menuIDList ?? []
+//    }
+//}
